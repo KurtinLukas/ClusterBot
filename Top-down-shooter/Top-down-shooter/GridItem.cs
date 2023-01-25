81feca92c519b6a8dc060e1a748812c47b2b812e
KurtinLukas <kurtinl.04@spst.eu>
2023-01-25 15:22:09 +0100

    Added some classes
    GridItem a Player

---
+++ Top-down-shooter/Top-down-shooter/GridItem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Top_down_shooter
+{
+    class GridItem
+    {
+        public int X;
+        public int Y;
+
+        public Material material;
+        public enum Material{
+            Air,
+            Wall,
+            Enemy,
+            Player,
+            Object //see-through non-walkable (e.g. barrel)
+        };
+
+        /// <summary>
+        /// Vytvoří nové grid pole
+        /// základní hodnota materiálu = Air
+        /// </summary>
+        /// <param name="posX">pozice X</param>
+        /// <param name="posY">pozice Y</param>
+        public GridItem(int posX, int posY)
+        {
+            X = posX;
+            Y = posY;
+            material = Material.Air;
+        }
+        /// <summary>
+        /// Vytvoří nové grid pole
+        /// základní hodnota materiálu = Air
+        /// </summary>
+        /// <param name="posX">pozice X</param>
+        /// <param name="posY">pozice Y</param>
+        /// <param name="mat">materiál (typ) pole</param>
+        public GridItem(int posX, int posY, Material mat)
+        {
+            X = posX;
+            Y = posY;
+            material = mat;
+        }
+    }
+}
---
+++ Top-down-shooter/Top-down-shooter/Player.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Top_down_shooter
+{
+    class Player
+    {
+        //proměnný
+        public Point position;
+        public int X = 0; //default position
+        public int Y = 0;
+        public int health = 100;
+
+        //  metody
+        //pohyb
+        public void MoveBy(int posX, int posY)
+        {
+            X = posX;
+            Y = posY;
+            position = new Point(posX, posY);
+        }
+        public void MoveBy(Point posPoint)
+        {
+            X = posPoint.X;
+            Y = posPoint.Y;
+            position = posPoint;
+        }
+
+        //akce
+        public void Shoot(Point direction)
+        {
+
+        }
+
+        public void TakeDamage(int value)
+        {
+            health -= value;
+        }
+
+        //ostatní
+
+
+        //deklarace
+        public Player()
+        {
+            position = new Point(X, Y);
+        }
+        public Player(int posX, int posY)
+        {
+            X = posX;
+            Y = posY;
+            position = new Point(posX, posY);
+        }
+        public Player(Point posPoint)
+        {
+            X = posPoint.X;
+            Y = posPoint.Y;
+            position = posPoint;
+        }
+    }
+}
