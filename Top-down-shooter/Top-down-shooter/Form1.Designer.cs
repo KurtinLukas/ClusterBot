d7dfa0dddcee4b5292a5d3ae3fc36b65c3bc63f0
matlj04 <matlj.04@spst.eu>
2023-01-26 19:45:09 +0100

    Basic movement a geometricke tvary

--- Top-down-shooter/Top-down-shooter/Form1.Designer.cs
+++ Top-down-shooter/Top-down-shooter/Form1.Designer.cs
@@ -30,22 +30,26 @@ namespace Top_down_shooter
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Location = new System.Drawing.Point(18, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.Size = new System.Drawing.Size(93, 37);
             this.label1.TabIndex = 0;
             this.label1.Text = "Kills: ";
             // 
@@ -53,9 +57,10 @@ namespace Top_down_shooter
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(300, 21);
+            this.label2.Location = new System.Drawing.Point(450, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(84, 25);
+            this.label2.Size = new System.Drawing.Size(129, 37);
             this.label2.TabIndex = 1;
             this.label2.Text = "Ammo: ";
             // 
@@ -63,23 +68,29 @@ namespace Top_down_shooter
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(594, 21);
+            this.label3.Location = new System.Drawing.Point(891, 32);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 25);
+            this.label3.Size = new System.Drawing.Size(127, 37);
             this.label3.TabIndex = 2;
             this.label3.Text = "Health: ";
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(686, 12);
+            this.progressBar1.Location = new System.Drawing.Point(1029, 18);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(286, 43);
+            this.progressBar1.Size = new System.Drawing.Size(429, 66);
             this.progressBar1.TabIndex = 3;
             this.progressBar1.Value = 100;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(396, 485);
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(594, 746);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(50, 50);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -88,22 +99,39 @@ namespace Top_down_shooter
             // 
             // timer1
             // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(-3, -2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1483, 1096);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.game_graphics);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 711);
+            this.ClientSize = new System.Drawing.Size(1476, 1094);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,6 +145,7 @@ namespace Top_down_shooter
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- Top-down-shooter/Top-down-shooter/Form1.cs
+++ Top-down-shooter/Top-down-shooter/Form1.cs
@@ -12,19 +12,72 @@ namespace Top_down_shooter
 {
     public partial class Form1 : Form
     {
+        Player player;
+        bool left = false;
+        bool right = false;
+        bool up = false;
+        bool down = false;
+        int speed = 10;
+
         public Form1()
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            player = new Player(500, 400);
+        }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-
+            if (left)
+                player.X -= speed;
+            if (right)
+                player.X += speed;
+            if (up)
+                player.Y -= speed;
+            if (down)
+                player.Y += speed;
+            if ((left && up) || (left && down) || (right && up) || (right && down))
+                speed = 5;
+            else
+                speed = 10;
+            pictureBox1.Location = new Point(player.X, player.Y);
+            pictureBox2.Invalidate();
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
+            if (e.KeyCode == Keys.A)
+                left = true;
+            if (e.KeyCode == Keys.D)
+                right = true;
+            if (e.KeyCode == Keys.W)
+                up = true;
+            if (e.KeyCode == Keys.S)
+                down = true;
+        }
 
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+                left = false;
+            if (e.KeyCode == Keys.D)
+                right = false;
+            if (e.KeyCode == Keys.W)
+                up = false;
+            if (e.KeyCode == Keys.S)
+                down = false;
+        }
+
+        private void game_graphics(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            graphics.Clear(Color.Black);
+            SolidBrush player_brush = new SolidBrush(Color.Red);
+            graphics.FillRectangle(player_brush, player.X, player.Y, 50, 50);
+            graphics.FillEllipse(new SolidBrush(Color.Yellow), player.X + 5, player.Y + 10, 10, 10);
+            graphics.FillEllipse(new SolidBrush(Color.Yellow), player.X + 50 - 20, player.Y + 10, 10, 10);
         }
     }
 }
