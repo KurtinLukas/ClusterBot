b30560be5dacd149ac0435626fa2d52fd7b2d1eb
KurtinLukas <kurtinl.04@spst.eu>
2023-01-29 21:58:59 +0100

    Bullet class

---
+++ Top-down-shooter/Top-down-shooter/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Top_down_shooter
+{
+    class Bullet
+    {
+        public double posX;
+        public double posY;
+        public double rotation;
+        private double moveX;
+        private double moveY;
+        
+        public Bullet(int x, int y, float rotation)
+        {
+            posX= x;
+            posY = y;
+            this.rotation = rotation;
+            moveX = x;
+            moveY = y;
+        }
+
+        public void Move()
+        {
+            moveX = Math.Cos(rotation) * 5f;
+            moveY = Math.Sin(rotation) * 5f;
+            posX -= (int)moveX;
+            posY -= (int)moveY;
+        }
+    }
+}
